f0201717a0d341fadda0f25a73556d87c0d63309
Anlesvavor <jesusjar32@live.com>
2016-10-24 19:57:20 -0600

    ayy 2

--- ColeccionDeVideojuegos/src/gui/FormularioVideojuegos.java
+++ ColeccionDeVideojuegos/src/gui/FormularioVideojuegos.java
@@ -16,8 +16,8 @@ public class FormularioVideojuegos extends javax.swing.JFrame {
      */
     public FormularioVideojuegos() {
         initComponents();
+//        ;)
     }
-
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
